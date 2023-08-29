9a8c74157a3d5bac5145cb49a8450735f19517e0
Mohamed2242 <98766960+Mohamed2242@users.noreply.github.com>
2023-08-30 02:35:53 +0300

    Add files via upload

---
+++ Navigation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_uber
+{
+    internal class Navigation
+    {
+        private TripTypeStrategy tripTypeStrategy;
+
+        public void setStrategy(TripTypeStrategy tripTypeStrategy)
+        {
+            this.tripTypeStrategy = tripTypeStrategy;
+        }
+
+        public void tripType(string start, string destination)
+        {
+            this.tripTypeStrategy.tripType(start, destination);
+        }
+    }
+}
